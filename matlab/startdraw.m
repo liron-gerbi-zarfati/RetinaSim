38923e4e9d0ae177e6d96a587f26855c62354629
yuval <yuval@bar-ilan>
2014-09-23 16:28:49 +0300

    added log and q for quit

--- matlab/startdraw.m
+++ matlab/startdraw.m
@@ -11,28 +11,58 @@ maxPix=56;
 minPix=9;
 %% run the experiment
 img_temp=uint8(zeros(size(img)));
+firstClick=true;
+logN=1; % file number to save, 1 for log1.mat
+log=[];;
+labels={'time(s) from first click','action','x','y','half square'};
+actions={'1 = click','2 = drag','3 = wheel up (zoom out)','4 = wheel down (zoom in)'};
 fh= figure('units','normalized','outerposition',[0 0 1 1],'toolbar','none','MenuBar','none');
 ha = axes('Xlim', [1 size(img,2)],'Ylim',[1 size(img,2)],'XTick',[],'YTick',[]);
 set(gca,'color','k')
 set(gcf,'color',[0.1 0.1 0.1])
 set(fh,'WindowButtonDownFcn',@Mouse_Press);
+set(fh,'WindowKeyPressFcn',@Key_Press);
+% current_key = double(get(fh,'CurrentCharacter'));
+% if current_key == 30 %UP arrow was pressed
+%        figure([0,2,0])
+% end
 uiwait(fh);
+
 %% callback functions
+    function Key_Press(~,keyData)
+        switch keyData.Key
+            case 'q'
+                while exist(['log',num2str(logN),'.mat'],'file')
+                    logN=logN+1;
+                end
+                save (['log',num2str(logN)],'log','actions','labels')
+                close
+                return
+        end
+    end
     function Mouse_Press(src,~)
         %set(src,'pointer','crosshair')
         cp = get(ha,'CurrentPoint');
         xinit = round(cp(1,1));
         yinit = round(cp(1,2));
+        if firstClick
+            tic
+            firstClick=false;
+            log=[0,1,xinit,yinit,pix]; % time,action,x,y
+        else
+            log(end+1,:)=[toc,1,xinit,yinit,pix];
+        end
         %       hl = line('XData',xinit,'YData',yinit,...
         %              'color','k','linewidth',2);
         plotSquare(xinit,yinit,pix,minColor,maxColor,src)
         set(src,'WindowButtonMotionFcn',@movedraw)
         set(src,'WindowButtonUpFcn',@enddraw)
         set(src,'WindowScrollWheelFcn',@zoom)
-        function movedraw(~,~) 
+        function movedraw(~,~)
             cp = get(ha,'CurrentPoint');
             xinit = round(cp(1,1));
             yinit = round(cp(1,2));
+            log(end+1,:)=[toc,2,xinit,yinit,pix];
             plotSquare(xinit,yinit,pix,minColor,maxColor,src)
             %               drawnow
         end
@@ -43,11 +73,18 @@ uiwait(fh);
             uiresume(fh);
         end
         function zoom(~,event)
-            if event.VerticalScrollCount < 0 && pix<maxPix%up
-                pix=pix+5;
-            end
-            if event.VerticalScrollCount > 0 && pix>minPix%down
-                pix=pix-5;
+            if event.VerticalScrollCount < 0 %up
+                log(end+1,:)=[toc,3,xinit,yinit,pix];
+                if pix<maxPix
+                    pix=pix+5;
+                    log(end,5)=pix;
+                end
+            elseif event.VerticalScrollCount > 0 %down
+                log(end+1,:)=[toc,4,xinit,yinit,pix];
+                if pix>minPix
+                    pix=pix-5;
+                    log(end,5)=pix;
+                end
             end
             plotSquare(xinit,yinit,pix,minColor,maxColor,src)
         end
