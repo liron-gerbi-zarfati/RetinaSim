1f304545987601c3fa2b848a5933253c360bd34f
yuval <yuval@bar-ilan>
2014-09-23 12:03:34 +0300

    zoom added

--- matlab/startdraw.m
+++ matlab/startdraw.m
@@ -1,13 +1,23 @@
 function  startdraw
-pix=30;
 img=rgb2gray(imread('peppers.png'));
-img_temp=zeros(size(img));
+img(:,:,2)=img;img(:,:,3)=img(:,:,1);
+%% set some parameters
+pix=30; % half of the square in pixels
+% color scale
+minColor=0; 
+maxColor=255;
+% when to ignore wheel (too large or too small pix)
+maxPix=56; 
+minPix=9;
+%% run the experiment
+img_temp=uint8(zeros(size(img)));
 fh= figure('units','normalized','outerposition',[0 0 1 1],'toolbar','none','MenuBar','none');
 ha = axes('Xlim', [1 size(img,2)],'Ylim',[1 size(img,2)],'XTick',[],'YTick',[]);
 set(gca,'color','k')
 set(gcf,'color',[0.1 0.1 0.1])
 set(fh,'WindowButtonDownFcn',@Mouse_Press);
 uiwait(fh);
+%% callback functions
     function Mouse_Press(src,~)
         %set(src,'pointer','crosshair')
         cp = get(ha,'CurrentPoint');
@@ -15,6 +25,35 @@ uiwait(fh);
         yinit = round(cp(1,2));
         %       hl = line('XData',xinit,'YData',yinit,...
         %              'color','k','linewidth',2);
+        plotSquare(xinit,yinit,pix,minColor,maxColor,src)
+        set(src,'WindowButtonMotionFcn',@movedraw)
+        set(src,'WindowButtonUpFcn',@enddraw)
+        set(src,'WindowScrollWheelFcn',@zoom)
+        function movedraw(~,~) 
+            cp = get(ha,'CurrentPoint');
+            xinit = round(cp(1,1));
+            yinit = round(cp(1,2));
+            plotSquare(xinit,yinit,pix,minColor,maxColor,src)
+            %               drawnow
+        end
+        function enddraw(src,~)
+            set(src,'Pointer','arrow')
+            set(src,'WindowButtonMotionFcn',[])
+            set(src,'WindowButtonUpFcn',[])
+            uiresume(fh);
+        end
+        function zoom(~,event)
+            if event.VerticalScrollCount < 0 && pix<maxPix%up
+                pix=pix+5;
+            end
+            if event.VerticalScrollCount > 0 && pix>minPix%down
+                pix=pix-5;
+            end
+            plotSquare(xinit,yinit,pix,minColor,maxColor,src)
+        end
+    end
+%% functions
+    function plotSquare(xinit,yinit,pix,minColor,maxColor,src)
         if xinit-pix<1
             xinit=pix+1;
         end
@@ -27,63 +66,13 @@ uiwait(fh);
         if yinit+pix>size(img,1)
             yinit=size(img,1)-pix;
         end
-        img_temp(yinit-pix:yinit+pix,xinit-pix:xinit+pix)=img(yinit-pix:yinit+pix,xinit-pix:xinit+pix);
-        imagesc(img_temp);
+        img_temp(yinit-pix:yinit+pix,xinit-pix:xinit+pix,1:3)=img(yinit-pix:yinit+pix,xinit-pix:xinit+pix,1:3);
+        imagesc(img_temp,[minColor maxColor])
+        %image(img_temp)
         % axis off
         set(gca,'xtick',[])
         set(gca,'ytick',[])
         colormap('gray');
-        img_temp=zeros(size(img));
-        
-        set(src,'WindowButtonMotionFcn',@movedraw)
-        set(src,'WindowButtonUpFcn',@enddraw)
-        set(src,'WindowScrollWheelFcn',@mytest)
-        function movedraw(~,~)
-            
-            cp = get(ha,'CurrentPoint');
-            xinit = round(cp(1,1)); yinit = round(cp(1,2));
-            
-            %               xdat = [get(hl,'XData'),cp(1,1)];
-            %               ydat = [get(hl,'YData'),cp(1,2)];
-            %               set(hl,'XData',xdat,'YData',ydat);
-            %               if xinit-10<0 &yinit-10<0
-            %               xinit=11;
-            %               yinit=11;
-            %               end
-            if xinit-pix<1
-                xinit=pix+1;
-            end
-            if yinit-pix<1
-                yinit=pix+1;
-            end
-            if xinit+pix>size(img,2)
-                xinit=size(img,2)-pix;
-            end
-            if yinit+pix>size(img,1)
-                yinit=size(img,1)-pix;
-            end
-            try
-                img_temp(yinit-pix:yinit+pix,xinit-pix:xinit+pix)=img(yinit-pix:yinit+pix,xinit-pix:xinit+pix);
-            catch
-                disp(num2str([yinit-pix yinit+pix xinit-pix xinit+pix]))
-            end
-            imagesc(img_temp)
-            %axis off
-            set(gca,'xtick',[])
-            set(gca,'ytick',[])
-            colormap('gray');
-            img_temp=zeros(size(img));
-            %               drawnow
-        end
-        function enddraw(src,~)
-            set(src,'Pointer','arrow')
-            set(src,'WindowButtonMotionFcn',[])
-            set(src,'WindowButtonUpFcn',[])
-            uiresume(fh);
-        end
-        function mytest(~,~)
-            %               figure;
-            %               bla lna nksanfjkhg;kf
-        end
+        img_temp=uint8(zeros(size(img)));
     end
 end
